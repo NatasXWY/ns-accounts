7eb135f0d2360e29ba347d5f65cbbeb9356492d3
xuwenyue <xuwenyue@rongcapital.cn>
2019-12-22 13:11:16 +0800

    初始化

---
+++ config/webpack.dev.js
@@ -0,0 +1,54 @@
+const path    = require('path');
+const merge   = require('webpack-merge');
+const common  = require('./webpack.common.js');
+const webpack = require('webpack');
+const theme   = require('../src/theme');
+
+module.exports = merge(common, {
+    mode      : 'development',
+    devtool   : 'inline-source-map',
+    devServer : {
+        contentBase : path.resolve(__dirname, '../dist'),
+        hot         : true,
+        port        : 9000,
+        overlay     : {
+            warnings : false,
+            errors   : true
+        }
+    },
+    plugins   : [
+        new webpack.HotModuleReplacementPlugin(),   //启用HMR,配合server的hot
+    ],
+    module    : {
+        rules : [
+            {
+                test : /\.scss$/,
+                use  : [
+                    'style-loader',
+                    'css-loader',
+                    'scss-loader'
+                ]
+            },
+            {
+                test : /\.css$/,
+                use  : [
+                    'style-loader',
+                    'css-loader',
+                    'postcss-loader'
+                ]
+            },
+            {
+                test : /\.less$/,
+                use  : [
+                    'style-loader',
+                    'css-loader',
+                    'postcss-loader',
+                    {
+                        loader  : 'less-loader',
+                        options : {'modifyVars' : theme, 'javascriptEnabled' : true}
+                    }
+                ]
+            }
+        ]
+    }
+});
